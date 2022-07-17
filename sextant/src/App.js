f089f4f6c1ee0911c146b5942794a706e67a077a
git-azeez <azeezahmad.sd@gmail.com>
2022-07-17 18:49:25 +0530

    created banner in the web page

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+
+        <
+        div className = "App" >
+        <
+        header className = "App-header" >
+        <
+        h1 > Hello CodeSandbox < /h1>
+        // <
+        // img src = { logo }
+        // className = "App-logo"
+        // alt = "logo" / >
+        // <
+        p >
+        Edit < code > "src / App.js"
+        Hello Azeez!!!You have to change in this mentioned dir and see the changes < /code> and save to reload. < /
+        p > <
+        a className = "App-link"
+        href = "https://reactjs.org"
+        target = "_blank"
+        rel = "noopener noreferrer" >
+        Learn React <
+        /a> < /
+        header > <
+        /div>
+    );
 }
 
 export default App;
